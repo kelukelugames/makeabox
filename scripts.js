70fca0bac54d5b46468d02c2574c5fbd2909f4ee
kelukelugames <kelukelugames@gmail.com>
2015-09-19 19:32:24 -0700

    finish a piece

--- scripts.js
+++ scripts.js
@@ -1,11 +1,30 @@
+$(document).ready(function() {
+    $(".piece").click(function() {
+        rotate(this);
+    });
+});
 
-function rotateDIV()
-{
-	var x=document.getElementById("rotate2D");
-	var n = (90 + parseInt(x.style["transform"] ? x.style["transform"].match(/\d+/)[0] : 0)) % 360;
+function flip(x) {
+    if ($(x).hasClass("flipped")) {
+        $(x).removeClass("flipped");
+        console.log("removed");
+    } else {
+        $(x).addClass("flipped");
+        console.log("added");
+    }
+}
+
+function rotate(x) {
+	var n = (90 + parseInt(x.style["transform"] ?
+                           x.style["transform"].match(/\d+/)[0] : 0)) % 360;
 	
 	x.style.transform = n;
 	x.style.webkitTransform = "rotate(" + n + "deg)"
 	x.style.OTransform = "rotate(" + n + "deg)"
 	x.style.MozTransform = "rotate(" + n + "deg)"
+    
+    if (n == 0) {
+        flip(x);
+    }
 }
+
